efebef45120ef4e14317809cf9a53eff97655c27
becca221b <chake_rebeca@hotmail.com>
2026-04-16 20:27:11 -0300

    Add Movement Model

---
+++ Models/Movement.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica1.Models
+{
+    public class Movement
+
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string Type { get; set; } // "Entrada" o "Salida"
+    }
+}
