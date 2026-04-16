fa9db5b3b4d5a3697ce20c6e5b8aeebda6c9a6ba
becca221b <chake_rebeca@hotmail.com>
2026-04-16 20:39:13 -0300

    Add DB Context

---
+++ Context/DbContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Practica1.Models;
+
+namespace Practica1.Context
+{
+    public class DbContext
+    {
+        
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+
+}
