9b0e898d1e9e54bf8e8d91a15920984b02df0a9f
Rebeca <chake_rebeca@hotmail.com>
2026-04-17 10:50:29 -0300

    Add StockService

---
+++ Services/StockService.cs
@@ -0,0 +1,36 @@
+﻿using Practica1.Models;
+using Practica1.Repositories;
+
+namespace Practica1.Services
+{
+    public class StockService
+    {
+        private readonly ProductRepository _productRepository;
+
+        public StockService(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void ApplyMovement(Movement movement) {
+            var product = _productRepository.GetProductById(movement.ProductId);
+
+            if (product == null) {
+                throw new Exception("Product not found");
+            }
+
+            if(movement.Type == "OUT" && product.Stock < movement.Quantity)
+            {
+                throw new Exception("No stock");
+            }
+            if(movement.Type == "IN")
+            {
+                product.Stock += movement.Quantity;
+            }
+            else
+            {
+                product.Stock -= movement.Quantity;
+            }
+        }
+    }
+}
